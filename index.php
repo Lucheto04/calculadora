e6f0923d8e92c5da0c2fdb9c2fe11f98704eef7c
lucho <lucho.lgrg04@gmail.com>
2023-05-30 17:32:30 -0500

    style: :lipstick: le añadimos estilos a los botones
    Le añadimos un borde, un sombreado, un color de fondo y le cambiamos el tamaño de la fuente a los botones

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php 
+    $nueve = (int)($_POST ["9"]);
+    $ocho = (int)($_POST ["8"]);
+    $siete = (int)($_POST ["7"]);
+    $seis = (int) ($_POST ["6"]);
+    $cinco = (int) ($_POST ["5"]);
+    $cuatro = (int) ($_POST ["4"]);
+    $tres = (int) ($_POST ["3"]);
+    $dos = (int) ($_POST ["2"]);
+    $uno = (int) ($_POST ["1"]);
+    $cero = (int) ($_POST ["0"]);
+
+    echo $dos;
+
     $numero1 = (float)($_POST ["numero1"]);
     $numero2 = (float)($_POST ["numero2"]);
     $operacion = ($_POST ["operadores"]);
     $resultado = "";
+
+     
     switch ($operacion) {
         case "+":
             $resultado = $numero1 + $numero2;
@@ -16,7 +31,8 @@
         case "/":
             $resultado = $numero1 / $numero2;
             break;
-    }
+    };
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,28 +48,28 @@
 
     <form  method="POST">
         <div>
-            <span><button value="7" class="boton">7</button></span>
-            <span><button value="7" class="boton">8</button></span>
-            <span><button value="7" class="boton">9</button></span>
-            <span><button value="-" class="boton">-</button></span>
+            <span><button value="7" name="7" class="boton">7</button></span>
+            <span><button value="8" name="8" class="boton">8</button></span>
+            <span><button value="9" name="9" class="boton">9</button></span>
+            <span><button value="+" name="+" class="boton">+</button></span>
         </div>
         <div>
-            <span><button value="7" class="boton">4</button></span>
-            <span><button value="7" class="boton">5</button></span>
-            <span><button value="7" class="boton">6</button></span>
-            <span><button value="*" class="boton">*</button></span>
+            <span><button value="4" name="4" class="boton">4</button></span>
+            <span><button value="5" name="5" class="boton">5</button></span>
+            <span><button value="6" name="6" class="boton">6</button></span>
+            <span><button value="-" name="-" class="boton">-</button></span>
         </div>
         <div>
-            <span><button value="7" class="boton">1</button></span>
-            <span><button value="7" class="boton">2</button></span>
-            <span><button value="7" class="boton">3</button></span>
-            <span><button value="/" class="boton">/</button></span>
+            <span><button value="1" name="1" class="boton">1</button></span>
+            <span><button value="2" name="2" class="boton">2</button></span>
+            <span><button value="3" name="3" class="boton">3</button></span>
+            <span><button value="*" name="*" class="boton">*</button></span>
         </div>
         <div>
-            <span><button value="7" class="boton">0</button></span>
-            <span><button value="7" class="boton">.</button></span>
-            <span><button value="7" class="boton">=</button></span>
-            <span><button value="/" class="boton">/</button></span>
+            <span><button value="0" name="0" class="boton">0</button></span>
+            <span><button value="." name="." class="boton">.</button></span>
+            <span><button value="=" name="=" class="boton">=</button></span>
+            <span><button value="/" name="/" class="boton">/</button></span>
         </div>
 
         <input type="number" name="numero1" class="formulario"> <br>
