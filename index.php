68bcd40d1b8aa18ad38e301cb5f39372e45bde19
Lucheto04 <124000317+Lucheto04@users.noreply.github.com>
2023-06-08 18:42:59 +0000

    feat: :sparkles: Hacemos varios cambios y le añadimos el metodo 'eval'
    Le resumimos el codigo a las operaciones añadiendo el metodo eval, tambien cambiamos lo que imprimimos en el textarea por el $display

--- index.php
+++ index.php
@@ -1,31 +1,47 @@
-<?php 
-    session_start();
-    $numero = (int)($_POST ["numero"]);
-    $deleteAll = $_POST['deleteAll'];
-    $delete1 = $_POST['delete1'];
-    $operador =(string) ($_POST ["operador"]);
-    $pi = (float) ($_POST ['PI']);
+<?php
+                session_start();
+$deleteAll = $_POST['deleteAll'];
+$delete1 = $_POST['delete1'];
 
-
-    if (isset($numero)) {
-        if ($deleteAll == "c") {
-            $_SESSION['num1'] = null;
-        } else if ($delete1 == "<=") {
-            $_SESSION['num1'] = substr($_SESSION['num1'],0, -1);
-        } else if (isset($_SESSION['num1'])) {
-            $_SESSION['num1'] .= $numero;
-        } else if ($operador == "+") {
-            $_SESSION['num1'] .= $operador;
-        }  
-        else {
-            $_SESSION['num1'] = $numero;
-            $_SESSION['num1'] = $pi;
-        }
-    }
+if (isset($_POST["numero"])) 
+    $numero = (int)$_POST["numero"];
     
-    echo $numero;
-    echo $operador;
+
+    if ($deleteAll == "c") {
+        $_SESSION['num1'] = "";
+    }
+    else if ($delete1 == "<=") {
+        $_SESSION['num1'] = substr($_SESSION['num1'],0, -1);
+    }
+    elseif (isset($_SESSION['num1'])) {
+        $_SESSION['num1'] .= $numero;
+    }
+    else {
+        $_SESSION['num1'] = $numero;
+    }
+
+
+if (isset($_POST["operador"])) {
+    $operador = $_POST["operador"];
+    $_SESSION['num1'] .= $operador;
+}
+
+if (isset($_POST["PI"])) {
+    $pi = (float)$_POST["PI"];
+    $_SESSION['num1'] .= $pi;
+}
+
+$display = isset($_SESSION['num1']) ? $_SESSION['num1'] : "";
+
+if (isset($_POST["resultado"])) {
+
+    $resultado = eval('return ' . $_SESSION['num1'] . ';');
+    $display .= "=" . $resultado;
+
+    unset($_SESSION['num1']);
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -38,17 +54,13 @@
 <body>
     <form  method="POST">
         <h1>Calculadora</h1>
-        <textarea name="" id="" cols="23" rows="2" type="text" placeholder="<?php  
-        echo (isset($_SESSION['num1'])) ? $_SESSION['num1'] : 0;  
-        echo ($operador);
-        ?>" class="formulario" disabled></textarea>
+        <textarea name="" id="" cols="23" rows="2" type="text" placeholder="<?php echo $display; ?>"  class="formulario" disabled></textarea>
         <div>
             <span><button value="7" name="numero" class="boton">7</button></span>
             <span><button value="8" name="numero" class="boton">8</button></span>
             <span><button value="9" name="numero" class="boton">9</button></span>
             <span><button value=" +" name="operador" class="botonOperador">+</button></span>
             <span><button value=" %" name="operador" class="botonOperador">%</button></span>
-
         </div>
         <div>
             <span><button value="4" name="numero" class="boton">4</button></span>
@@ -56,7 +68,6 @@
             <span><button value="6" name="numero" class="boton">6</button></span>
             <span><button value=" -" name="operador" class="botonOperador">-</button></span>
             <span><button value="<=" name="delete1" class="botonDelete">←</button></span>
-
         </div>
         <div>
             <span><button value="1" name="numero" class="boton">1</button></span>
@@ -64,7 +75,6 @@
             <span><button value="3" name="numero" class="boton">3</button></span>
             <span><button value=" *" name="operador" class="botonOperador">*</button></span>
             <span><button value="c" name="deleteAll" class="botonDelete">c</button></span>
-
         </div>
         <div>
             <span><button value="0" name="numero" class="boton">0</button></span>
@@ -72,12 +82,7 @@
             <span><button value="3.1415" name="PI" class="boton">n</button></span>
             <span><button value=" /" name="operador" class="botonOperador">/</button></span>
             <span><button value="=" name="resultado" class="botonResultado">=</button></span>
-
         </div>
-
-
     </form>
-
-
 </body>
 </html>
